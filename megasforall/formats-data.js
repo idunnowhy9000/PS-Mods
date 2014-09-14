7dfbdd9294e07d5fd28de9c2df753bddc9a7f8d7
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-09-14 11:06:43 +0700

    Update M4A's formats-data.js

--- megasforall/formats-data.js
+++ megasforall/formats-data.js
@@ -15,4 +15,79 @@ exports.BattleFormatsData = {
 		inherit: true,
 		tier: "OU"
 	},
+	accelgormega: {
+		tier: "OU"
+	},
+	alomomolamega: {
+		tier: "OU"
+	},
+	amoongussmega: {
+		tier: "OU"
+	},
+	arbokmega: {
+		tier: "OU"
+	},
+	aegislashmega: {
+		tier: "OU"
+	},
+	archeopsmega: {
+		tier: "OU"
+	},
+	arcaninemegax: {
+		tier: "OU"
+	},
+	arcaninemegay: {
+		tier: "OU"
+	},
+	articunomegax: {
+		tier: "OU"
+	},
+	articunomegay: {
+		
+		tier: "OU"
+	},
+	audinomega: {
+		
+		tier: "OU"
+	},
+	aurorusmega: {
+		
+		tier: "OU"
+	},
+	avaluggmega: {
+		
+		tier: "OU"
+	},
+	azelfmega: {
+		
+		tier: "OU"
+	},
+	bibarelmega: {
+		
+		tier: "OU"
+	},
+	conkeldurrmega: {
+		
+		tier: "OU"
+	},
+	basculinmegax: {
+		
+		tier: "OU"
+	},
+	basculinmegay: {
+		
+		tier: "OU"
+	},
+	bastiodonmega: {
+		
+		tier: "OU"
+	},
+	butterfreemega: {
+		
+		tier: "OU"
+	},
+	blisseymega: {
+		
+		tier: "OU"
+	},
 }
