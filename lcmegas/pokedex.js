9661ddf3b0d8eeb44563d12633d8cb3feaf40eb6
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-09-27 17:25:51 +0700

    Add LC Megas

---
+++ lcmegas/pokedex.js
@@ -0,0 +1,17 @@
+exports.BattlePokedex = {
+	"bulbasaurmega": {
+		"species": "Bulbasaur-Mega",
+		"baseSpecies": "Bulbasaur",
+		"baseStats": {"hp": 45, "atk": 58, "def": 69, "spa": 76, "spd": 75, "spe": 45},
+		"types": ["Grass","Poison"],
+		"abilities": {"0": "Thick Fat"},
+		genderRatio:{M:0.875,F:0.125},heightm:0.7,weightkg:6.9,color:"Green",evos:["ivysaur"],eggGroups:["Monster","Grass"],
+		forme:"Mega",formeLetter:"M",types:["Fire","Dragon"]
+	},
+	"charmandermegax": {
+		"species": "Charmander-Mega-X",
+		"baseSpecies": "Charmander",
+		forme:"Mega-X",formeLetter:"M",types:["Fire","Dragon"],
+		genderRatio:{M:0.875,F:0.125},baseStats:{hp:39,atk:75,def:76,spa:70,spd:50,spe:65},abilities:{0:"Tough Claws"},heightm:1.7,weightkg:110.5,color:"Red",prevo:"charmeleon",evoLevel:36,eggGroups:["Monster","Dragon"]
+	},
+}
