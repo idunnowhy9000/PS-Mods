96ce9ca78d9078818c6dd316173fb0fff4ab9531
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-10-02 20:50:52 +0700

    Update Pokemon Minus

---
+++ pokemonminus/abilities.js
@@ -0,0 +1,31 @@
+exports.BattleAbilities = {
+	"rational": {
+		desc: "Will raise the user's Sp.Atk stat one level when hit by any Dark-type moves. Unlike other abilities with immunity to certain typed moves, the user will still receive damage from the attack. Justified will raise Attack one level for each hit of a multi-hit move like Beat Up.",
+		shortDesc: "This Pokemon's Sp.Atk is boosted by 1 after it is damaged by a Dark-type attack.",
+		onAfterDamage: function (damage, target, source, effect) {
+			if (effect && effect.type === 'Dark') {
+				this.boost({spa:1});
+			}
+		},
+		id: "rational",
+		name: "rational",
+		rating: 2,
+		num: -100
+	},
+	"unnerve": {
+		inherit: true,
+		onStart: function (pokemon) {
+			var foeactive = pokemon.side.foe.active;
+			for (var i = 0; i < foeactive.length; i++) {
+				if (!foeactive[i] || !this.isAdjacent(foeactive[i], pokemon)) continue;
+				if (foeactive[i].volatiles['substitute']) {
+					// does it give a message?
+					this.add('-activate', foeactive[i], 'Substitute', 'ability: Unnerve', '[of] ' + pokemon);
+				} else {
+					this.add('-ability', pokemon, 'Unnerve', '[of] ' + foeactive[i]);
+					this.boost({spa: -1}, foeactive[i], pokemon);
+				}
+			}
+		},
+	}
+}
---
+++ pokemonminus/scripts.js
@@ -0,0 +1,24 @@
+exports.BattleScripts = {
+	init: function() {
+		// gengar
+		this.modData('Pokedex', 'gengar').baseStats = {hp:80,atk:95,def:95,spa:95,spd:100,spe:130};
+		this.modData('Pokedex', 'gengar').abilities = {0:'Infiltrator',1:'Prankster'};
+		this.modData('Learnsets', 'gengar').learnset.encore = ['6L1'];
+		// gengar-mega
+		this.modData('Pokedex', 'gengarmega').baseStats = {hp:80,atk:120,def:110,spa:180,spd:115,spe:140};
+		// kyogre
+		this.modData('Pokedex', 'kyogre').baseStats = {hp:130,atk:100,def:100,spa:150,spd:130,spe:90};
+		this.modData('Pokedex', 'kyogre').types = ['Water', 'Ice'];
+		//this.modData('Learnsets', 'kyogre').learnset.originpulse = ['6L1'];
+		// mewtwo
+		this.modData('Pokedex', 'mewtwo').baseStats = {hp:116,atk:120,def:90,spa:164,spd:90,spe:140};
+		// mewtwo-mega-x
+		this.modData('Pokedex', 'mewtwomegax').baseStats = {hp:116,atk:204,def:115,spa:130,spd:115,spe:140};
+		// mewtwo-mega-y
+		this.modData('Pokedex', 'mewtwomegay').baseStats = {hp:116,atk:130,def:90,spa:204,spd:120,spe:160};
+		this.modData('Pokedex', 'mewtwomegay').abilities[0] = "Rational";
+		// xerneas
+		this.modData('Pokedex', 'xerneas').baseStats = {hp:136,atk:131,def:95,spa:151,spd:108,spe:99};
+		this.modData('Pokedex', 'xerneas').types = ['Fairy', 'Grass'];
+	}
+}
