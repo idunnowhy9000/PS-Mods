eaec4b73eeeb08d4f8de5437af398184c0fbc47f
macrarazy <alitar085@gmail.com>
2014-10-05 13:48:33 +0500

    Update items.js

---
+++ moreweather/items.js
@@ -0,0 +1,101 @@
+exports.BattleItems = {
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+	"icyrock": {
+		id: "icyrock",
+		name: "Icy Rock",
+		spritenum: 221,
+		fling: {
+			basePower: 40
+		},
+		num: 282,
+		gen: 4,
+		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+	},
+};
