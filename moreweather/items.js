90e767462199f6aba1ec5f473a58e45947d0155e
macrarazy <alitar085@gmail.com>
2014-10-05 14:05:20 +0500

    Coded in Rock items (MW)

--- moreweather/items.js
+++ moreweather/items.js
@@ -1,101 +1,134 @@
 exports.BattleItems = {
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"cursedrock": {
+		id: "cursedrock",
+		name: "Cursed Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 684,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Sinister Fog lasts 8 turns instead of 5."
 	},
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"darkrock": {
+		id: "darkrock",
+		name: "Dark Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 685,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Black Hole lasts 8 turns instead of 5."
 	},
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"dracorock": {
+		id: "dracorock",
+		name: "Draco Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 686,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Dragon Meteor lasts 8 turns instead of 5."
 	},
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"fightingrock": {
+		id: "fightingrock",
+		name: "Fighting Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 687,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Warzone lasts 8 turns instead of 5."
 	},
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"galerock": {
+		id: "galerock",
+		name: "Gale Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 688,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Delta Stream lasts 8 turns instead of 5."
 	},
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"mossyrock": {
+		id: "mossyrock",
+		name: "Mossy Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 689,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Pollen Storm lasts 8 turns instead of 5."
 	},
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"pixierock": {
+		id: "pixierock",
+		name: "Pixie Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 690,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Pixie Fog lasts 8 turns instead of 5."
 	},
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"regularrock": {
+		id: "regularrock",
+		name: "Regular Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 691,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Clear Skies lasts 8 turns instead of 5."
 	},
-	"icyrock": {
-		id: "icyrock",
-		name: "Icy Rock",
-		spritenum: 221,
+	"steelrock": {
+		id: "steelrock",
+		name: "Steel Rock",
+		spritenum: 0,
 		fling: {
 			basePower: 40
 		},
-		num: 282,
+		num: 692,
 		gen: 4,
-		desc: "Holder's use of Hail lasts 8 turns instead of 5."
+		desc: "Holder's use of Metal Meteor lasts 8 turns instead of 5."
 	},
+	"stickyrock": {
+		id: "stickyrock",
+		name: "Sticky Rock",
+		spritenum: 0,
+		fling: {
+			basePower: 40
+		},
+		num: 693,
+		gen: 4,
+		desc: "Holder's use of Plague lasts 8 turns instead of 5."
+	},
+	"toxicrock": {
+		id: "toxicrock",
+		name: "Toxic Rock",
+		spritenum: 0,
+		fling: {
+			basePower: 40
+		},
+		num: 694,
+		gen: 4,
+		desc: "Holder's use of Acid Rain lasts 8 turns instead of 5."
+	},
+	"voltrock": {
+		id: "voltrock",
+		name: "Volt Rock",
+		spritenum: 0,
+		fling: {
+			basePower: 40
+		},
+		num: 695,
+		gen: 4,
+		desc: "Holder's use of Thunderstorm lasts 8 turns instead of 5."
+	}
 };
