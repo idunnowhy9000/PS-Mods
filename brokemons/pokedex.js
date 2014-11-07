6ac44efc0438c434aa737d30c7c887a4e4543d2e
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-11-07 14:10:31 +0700

    Update Brokemons; add Immunimons

---
+++ brokemons/pokedex.js
@@ -0,0 +1,7 @@
+exports.BattlePokedex = {
+	venusaur: {
+		inherit: true,
+		baseStats: {hp:120,atk:100,def:100,spa:120,spd:110,spe:60},
+		abilities: {0: "Overgrow", 1: "Thick Fat", "H": "Solar Panel"}
+	}
+}
