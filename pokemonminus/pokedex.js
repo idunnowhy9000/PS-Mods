303d9357c614e157674342710d4407a50922d0ef
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-11-16 09:51:40 +0700

    Port Pokemon Minus' scripts.js to pokedex.js

--- pokemonminus/pokedex.js
+++ pokemonminus/pokedex.js
@@ -1,1 +1,267 @@
-// todo: port scripts.js to pokedex.js
+exports.BattlePokedex = {
+	// Slate 0
+	gengar: {
+		inherit: true,
+		baseStats: {
+			'hp': 80,
+			'atk': 95,
+			'def': 95,
+			'spa': 95,
+			'spd': 100,
+			'spe': 130,
+		},
+		abilities: {
+			0: 'Infiltrator',
+			1: 'Prankster',
+		}
+	},
+	gengarmega: {
+		inherit: true,
+		baseStats: {
+			'hp': 80,
+			'atk': 120,
+			'def': 110,
+			'spa': 180,
+			'spd': 115,
+			'spe': 140,
+		}
+	},
+	kyogre: {
+		inherit: true,
+		baseStats: {
+			'hp': 130,
+			'atk': 100,
+			'def': 100,
+			'spa': 150,
+			'spd': 130,
+			'spe': 90,
+		},
+		types: ['Water', 'Ice']
+	},
+	mewtwo:{
+		inherit: true,
+		baseStats: {
+			'hp': 116,
+			'atk': 120,
+			'def': 90,
+			'spa': 164,
+			'spd': 90,
+			'spe': 140,
+		}
+	},
+	mewtwomegax: {
+		inherit: true,
+		baseStats: {
+			'hp': 116,
+			'atk': 204,
+			'def': 115,
+			'spa': 130,
+			'spd': 115,
+			'spe': 140,
+		}
+	},
+	mewtwomegay: {
+		inherit: true,
+		baseStats: {
+			'hp': 116,
+			'atk': 130,
+			'def': 90,
+			'spa': 204,
+			'spd': 120,
+			'spe': 160,
+		},
+		abilities: {
+			0: 'Rational'
+		}
+	},
+	// Slate 1
+	darkrai: {
+		inherit: true,
+		baseStats: {
+			'hp': 90,
+			'atk': 100,
+			'def': 90,
+			'spa': 145,
+			'spd': 90,
+			'spe': 125,
+		}
+	},
+	dialga: {
+		inherit: true,
+		baseStats: {
+			'hp': 100,
+			'atk': 120,
+			'def': 120,
+			'spa': 150,
+			'spd': 100,
+			'spe': 90,
+		},
+		abilities: {
+			0: 'Pressure'
+			1: 'Regenerator',
+		}
+	},
+	yveltal: {
+		inherit: true,
+		baseStats: {
+			'hp': 136,
+			'atk': 136,
+			'def': 100,
+			'spa': 136,
+			'spd': 100,
+			'spe': 109,
+		},
+	},
+	// Slate 2
+	// Slate 3
+	gothitelle: {
+		inherit: true,
+		baseStats: {
+			'hp': 100,
+			'atk': 55,
+			'def': 110,
+			'spa': 110,
+			'spd': 130,
+			'spe': 95,
+		},
+		abilities: {
+			0: 'Clairvoyant Body'
+			1: 'Competitive',
+			'H': 'Shadow Tag'
+		},
+		types: ["Psychic","Dark"]
+	},
+	lugia: {
+		inherit: true,
+		baseStats: {
+			'hp': 111,
+			'atk': 90,
+			'def': 133,
+			'spa': 107,
+			'spd': 179,
+			'spe': 110,
+		},
+		types: ["Flying","Water"]
+	},
+	rayquaza: {
+		inherit: true,
+		baseStats: {
+			'hp': 105,
+			'atk': 160,
+			'def': 90,
+			'spa': 160,
+			'spd': 90,
+			'spe': 115,
+		}
+	},
+	scizor: {
+		inherit: true,
+		baseStats: {
+			'hp': 100,
+			'atk': 140,
+			'def': 110,
+			'spa': 130,
+			'spd': 110,
+			'spe': 115,
+		},
+		abilities: {
+			0: 'Swarm'
+			1: 'Iron Fist',
+			'H': 'Light Metal'
+		}
+	},
+	scizormega: {
+		inherit: true,
+		baseStats: {
+			'hp': 100,
+			'atk': 165,
+			'def': 155,
+			'spa': 65,
+			'spd': 125,
+			'spe': 115,
+		},
+		abilities: {
+			0: 'Tough Claws'
+		}
+	},
+	// Slate 4
+	genesect: {
+		inherit: true,
+		baseStats: {
+			'hp': 71,
+			'atk': 130,
+			'def': 110,
+			'spa': 130,
+			'spd': 110,
+			'spe': 119,
+		},
+		abilities: {
+			0: 'Download',
+			1: 'Light Metal',
+			'H': 'Mega Launcher'
+		}
+	},
+	sylveon: {
+		inherit: true,
+		baseStats: {
+			'hp': 130,
+			'atk': 65,
+			'def': 105,
+			'spa': 140,
+			'spd': 150,
+			'spe': 60,
+		},
+		abilities: {
+			0: 'Fairy Aura',
+			1: 'Magic Guard',
+			'H': 'Pixilate'
+		}
+	},
+	thundurus: {
+		inherit: true,
+		baseStats: {
+			'hp': 99,
+			'atk': 115,
+			'def': 80,
+			'spa': 140,
+			'spd': 100,
+			'spe': 136,
+		},
+		abilities: {
+			0: 'Competitive',
+			1: 'Prankster',
+			'H': 'Amplifier'
+		}
+	},
+	thundurustherian: {
+		inherit: true,
+		baseStats: {
+			'hp': 79,
+			'atk': 150,
+			'def': 95,
+			'spa': 145,
+			'spd': 80,
+			'spe': 121,
+		},
+		abilities: {
+			0: 'Defiant',
+			1: 'Generate',
+			'H': 'Multiscale'
+		}
+	},
+	wobbuffet: {
+		inherit: true,
+		baseStats: {
+			'hp': 255,
+			'atk': 33,
+			'def': 63,
+			'spa': 33,
+			'spd': 63,
+			'spe': 93,
+		},
+		abilities: {
+			0: 'Shadow Tag',
+			1: 'Natural Cure',
+		}
+	},
+}
--- pokemonminus/scripts.js
+++ pokemonminus/scripts.js
@@ -1,44 +1,12 @@
 exports.BattleScripts = {
 	init: function() {
 		// gengar
-		this.modData('Pokedex', 'gengar').baseStats['hp'] = 80;
-		this.modData('Pokedex', 'gengar').baseStats['atk'] = 95;
-		this.modData('Pokedex', 'gengar').baseStats['def'] = 95;
-		this.modData('Pokedex', 'gengar').baseStats['spa'] = 95;
-		this.modData('Pokedex', 'gengar').baseStats['spd'] = 100;
-		this.modData('Pokedex', 'gengar').baseStats['spe'] = 130;
-		this.modData('Pokedex', 'gengar').abilities = {0:'Infiltrator',1:'Prankster'};
 		this.modData('Learnsets', 'gengar').learnset.encore = ['6L1'];
 		// gengar-mega
-		this.modData('Pokedex', 'gengarmega').baseStats['hp'] = 80;
-		this.modData('Pokedex', 'gengarmega').baseStats['atk'] = 120;
-		this.modData('Pokedex', 'gengarmega').baseStats['def'] = 110;
-		this.modData('Pokedex', 'gengarmega').baseStats['spa'] = 180;
-		this.modData('Pokedex', 'gengarmega').baseStats['spd'] = 115;
-		this.modData('Pokedex', 'gengarmega').baseStats['spe'] = 140;
 		// kyogre
-		this.modData('Pokedex', 'kyogre').baseStats['hp'] = 130;
-		this.modData('Pokedex', 'kyogre').baseStats['atk'] = 100;
-		this.modData('Pokedex', 'kyogre').baseStats['def'] = 100;
-		this.modData('Pokedex', 'kyogre').baseStats['spa'] = 150;
-		this.modData('Pokedex', 'kyogre').baseStats['spd'] = 130;
-		this.modData('Pokedex', 'kyogre').baseStats['spe'] = 90;
-		this.modData('Pokedex', 'kyogre').types = ['Water', 'Ice'];
 		this.modData('Learnsets', 'kyogre').learnset.originpulse = ['6L1'];
 		// mewtwo
-		this.modData('Pokedex', 'mewtwo').baseStats['hp'] = 116;
-		this.modData('Pokedex', 'mewtwo').baseStats['atk'] = 120;
-		this.modData('Pokedex', 'mewtwo').baseStats['def'] = 90;
-		this.modData('Pokedex', 'mewtwo').baseStats['spa'] = 164;
-		this.modData('Pokedex', 'mewtwo').baseStats['spd'] = 90;
-		this.modData('Pokedex', 'mewtwo').baseStats['spe'] = 140;
 		// mewtwo-mega-x
-		this.modData('Pokedex', 'mewtwomegax').baseStats['hp'] = 116;
-		this.modData('Pokedex', 'mewtwomegax').baseStats['atk'] = 204;
-		this.modData('Pokedex', 'mewtwomegax').baseStats['def'] = 115;
-		this.modData('Pokedex', 'mewtwomegax').baseStats['spa'] = 130;
-		this.modData('Pokedex', 'mewtwomegax').baseStats['spd'] = 115;
-		this.modData('Pokedex', 'mewtwomegax').baseStats['spe'] = 140;
 		// mewtwo-mega-y
 		this.modData('Pokedex', 'mewtwomegay').baseStats['hp'] = 116;
 		this.modData('Pokedex', 'mewtwomegay').baseStats['atk'] = 130;
@@ -231,16 +199,15 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'dialga').learnset.morningsun = ['6L1'];
 		this.modData('Learnsets', 'dialga').learnset.calmmind = ['6L1'];
 		// yveltal
-		this.modData('Pokedex', 'yveltal').baseStats['hp'] = 136;
-		this.modData('Pokedex', 'yveltal').baseStats['atk'] = 136;
-		this.modData('Pokedex', 'yveltal').baseStats['def'] = 100;
-		this.modData('Pokedex', 'yveltal').baseStats['spa'] = 136;
-		this.modData('Pokedex', 'yveltal').baseStats['spd'] = 100;
-		this.modData('Pokedex', 'yveltal').baseStats['spe'] = 109;
 		this.modData('Learnsets', 'yveltal').learnset.knockoff = ['6L1'];
 		this.modData('Learnsets', 'yveltal').learnset.bravebird = ['6L1'];
 		this.modData('Learnsets', 'yveltal').learnset.nastyplot = ['6L1'];
 		this.modData('Learnsets', 'yveltal').learnset.willowisp = ['6L1'];
+		// genesect
+		// sylveon
+		// thundurus
+		// thundurus-t
+		// wobbuffet
 		// Every hidden ability becomes released
 		for (var i in this.data.FormatsData) {
 			this.modData('FormatsData', i).unreleasedHidden = false;
