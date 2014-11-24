b9fbdc4cb4097d30c0b7c74bf97675ffa6e9c80f
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-11-24 12:00:26 +0700

    Update More Weather

---
+++ moreweather/battle.js
@@ -0,0 +1,23 @@
+/* Copy this code to battle.js file in your
+ * client's folder after line 2662 ("}") and before
+ * line 2663 ("};")
+ * (Add a comma after line 2662 in "}")
+*/
+clearskies: {
+	name: 'Clear Skies',
+	startMessage: '', // todo
+	abilityMessage: "'s Cloudy cleared the sky!",
+	endMessage: 'Clouds began to cover the skies.'
+},
+warzone: {
+	name: 'Warzone',
+	startMessage: '', // todo
+	abilityMessage: '', // todo
+	endMessage: ''
+},
+acidrain: {
+	name: 'Acid Rain',
+	startMessage: 'It began to rain acidicly!',
+	abilityMessage: "'s Acid Warning whipped an acidic rain!",
+	endMessage: 'The acid rain lifted.'
+}
