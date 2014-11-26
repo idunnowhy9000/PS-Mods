3a6ee794a1a00bc6e34820b6f949870709aa53d1
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-11-26 16:56:59 +0700

    Add M4A's Learnsets

--- megasforall/v2/scripts.js
+++ megasforall/v2/scripts.js
@@ -35,6 +35,40 @@ exports.BattleScripts = {
 	},
 	init: function () {
 		// learnsets
+		this.modData('Learnsets', 'accelgor').learnset.toxicspikes = ['6L1'];
+		this.modData('Learnsets', 'amoonguss').learnset.darkpulse = ['6L1'];
+		this.modData('Learnsets', 'arbok').learnset.dragondance = ['6L1'];
+		this.modData('Learnsets', 'arbok').learnset.dragonrush = ['6L1'];
+		this.modData('Learnsets', 'arbok').learnset.venomdrench = ['6L1'];
+		this.modData('Learnsets', 'arcanine').learnset.batonpass = ['6L1'];
+		this.modData('Learnsets', 'arcanine').learnset.healingwish = ['6L1'];
+		this.modData('Learnsets', 'arcanine').learnset.nobleroar = ['6L1'];
+		this.modData('Learnsets', 'arcanine').learnset.blazekick = ['6L1'];
+		this.modData('Learnsets', 'articuno').learnset.aeroblast = ['6L1'];
+		this.modData('Learnsets', 'articuno').learnset.waterpulse = ['6L1'];
+		this.modData('Learnsets', 'articuno').learnset.shadowball = ['6L1'];
+		this.modData('Learnsets', 'audino').learnset.chatter = ['6L1'];
+		this.modData('Learnsets', 'audino').learnset.boomburst = ['6L1'];
+		this.modData('Learnsets', 'audino').learnset.bugbuzz = ['6L1'];
+		this.modData('Learnsets', 'aurorus').learnset.moonlight = ['6L1'];
+		this.modData('Learnsets', 'aurorus').learnset.powergem = ['6L1'];
+		this.modData('Learnsets', 'avalugg').learnset.heavyslam = ['6L1'];
+		this.modData('Learnsets', 'bibarel').learnset.stealthrock = ['6L1'];
+		this.modData('Learnsets', 'bibarel').learnset.slackoff = ['6L1'];
+		this.modData('Learnsets', 'basculin').learnset.closecombat = ['6L1'];
+		this.modData('Learnsets', 'basculin').learnset.vacuumwave = ['6L1'];
+		this.modData('Learnsets', 'basculin').learnset.poisonfang = ['6L1'];
+		this.modData('Learnsets', 'basculin').learnset.coil = ['6L1'];
+		this.modData('Learnsets', 'basculin').learnset.icefang = ['6L1'];
+		this.modData('Learnsets', 'beedrill').learnset.megahorn = ['6L1'];
+		this.modData('Learnsets', 'beedrill').learnset.bravebird = ['6L1'];
+		this.modData('Learnsets', 'beheeyem').learnset.flashcannon = ['6L1'];
+		this.modData('Learnsets', 'bellossom').learnset.quiverdance = ['6L1'];
+		this.modData('Learnsets', 'bellossom').learnset.flamethrower = ['6L1'];
+		this.modData('Learnsets', 'bellossom').learnset.fireblast = ['6L1'];
+		this.modData('Learnsets', 'bellossom').learnset.moonblast = ['6L1'];
+		this.modData('Learnsets', 'bellossom').learnset.drainingkiss = ['6L1'];
+		this.modData('Learnsets', 'blissey').learnset.moonblast = ['6L1'];
 		// hardcoded shit
 		var realMegas = {'venusaurmega':1,'charizardmegax':1,'charizardmegay':1,'blastoisemega':1,'alakazammega':1,'gengarmega':1,'kangaskhanmega':1,'pinsirmega':1,'gyaradosmega':1,'aerodactylmega':1,'mewtwomegax':1,'mewtwomegay':1,'ampharosmega':1,'scizormega':1,'heracrossmega':1,'houndoommega':1,'tyranitarmega':1,'blazikenmega':1,'gardevoirmega':1,'mawilemega':1,'aggronmega':1,'medichammega':1,'manectricmega':1,'banettemega':1,'absolmega':1,'latiasmega':1,'latiosmega':1,'garchompmega':1,'lucariomega':1,'abomasnowmega':1};
 		for (var i in this.data.Pokedex) {
