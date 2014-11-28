b966fa53effe7e0816083de340ad4159a355eb12
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-11-28 11:11:04 +0700

    Fix M4A

--- megasforall/v2/scripts.js
+++ megasforall/v2/scripts.js
@@ -70,13 +70,9 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'bellossom').learnset.drainingkiss = ['6L1'];
 		this.modData('Learnsets', 'blissey').learnset.moonblast = ['6L1'];
 		// hardcoded shit
-		var realMegas = {'venusaurmega':1,'charizardmegax':1,'charizardmegay':1,'blastoisemega':1,'alakazammega':1,'gengarmega':1,'kangaskhanmega':1,'pinsirmega':1,'gyaradosmega':1,'aerodactylmega':1,'mewtwomegax':1,'mewtwomegay':1,'ampharosmega':1,'scizormega':1,'heracrossmega':1,'houndoommega':1,'tyranitarmega':1,'blazikenmega':1,'gardevoirmega':1,'mawilemega':1,'aggronmega':1,'medichammega':1,'manectricmega':1,'banettemega':1,'absolmega':1,'latiasmega':1,'latiosmega':1,'garchompmega':1,'lucariomega':1,'abomasnowmega':1};
 		for (var i in this.data.Pokedex) {
 			var template = this.getTemplate(i);
 			var baseTemplate = this.getTemplate(template.baseSpecies);
-			if (!i in realMegas) {
-				this.modData('FormatsData', i).tier = 'M4A';
-			}
 			this.modData('Pokedex', i).weightkg = template.weightkg || baseTemplate.weightkg;
 			this.modData('Pokedex', i).heightm = baseTemplate.weightkg;
 		}
