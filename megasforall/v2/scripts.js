71f66df71d043ae225844f14f05b3cc4f3c627c9
idunnowhy9000 <idunnowhy9000@users.noreply.github.com>
2014-11-28 17:05:13 +0700

    Update scripts.js

--- megasforall/v2/scripts.js
+++ megasforall/v2/scripts.js
@@ -1,23 +1,31 @@
 exports.BattleScripts = {
 	runMegaEvo: function (pokemon) {
 		var side = pokemon.side;
-		var item = this.getItem(pokemon.item);
-		if (!item.megaStone) return false;
 		if (side.megaEvo) return false;
-		var template = this.getTemplate(item.megaStone);
-		if (!template.isMega) return false;
-		if (pokemon.baseTemplate.baseSpecies !== template.baseSpecies) {
-			var formTemp = false;
-			for (var i in pokemon.baseTemplate.otherFormes) {
-				formTemp = this.getTemplate(pokemon.baseTemplate.otherFormes[i]);
-				if (formTemp.isMega) {
-					if ((formTemp.forme === 'Mega-X' && item.id === 'charizarditex') || (formTemp.forme === 'Mega-Y' && item.id === 'charizarditey') || (formTemp.forme === 'Mega' && item.id === 'abomasite')) {
-						template = formTemp;
-						break;
-					}
+
+		var otherForme;
+		var template;
+		if (pokemon.baseTemplate.otherFormes) otherForme = this.getTemplate(pokemon.baseTemplate.otherFormes[0]);
+		if (otherForme && otherForme.isMega && otherForme.requiredMove) {
+			if (pokemon.moves.indexOf(toId(otherForme.requiredMove)) < 0) return false;
+			template = otherForme;
+		} else {
+			var item = this.getItem(pokemon.item);
+			if (!item.megaStone) return false;
+			template = this.getTemplate(item.megaStone);
+			if (pokemon.baseTemplate.baseSpecies !== template.baseSpecies) {
+				if (item.m4aEvolves && this.getTemplate(pokemon.baseTemplate.baseSpecies + item.m4aEvolves).exists) {
+					template = this.getTemplate(pokemon.baseTemplate.baseSpecies + item.m4aEvolves);
 				}
 			}
-			if (!formTemp) return false;
+		}
+		if (!template.isMega) return false;
+
+		var foeActive = side.foe.active;
+		for (var i = 0; i < foeActive.length; i++) {
+			if (foeActive[i].volatiles['skydrop'] && foeActive[i].volatiles['skydrop'].source === pokemon) {
+				return false;
+			}
 		}
 
 		// okay, mega evolution is possible
@@ -26,8 +34,10 @@ exports.BattleScripts = {
 		pokemon.details = template.species + (pokemon.level === 100 ? '' : ', L' + pokemon.level) + (pokemon.gender === '' ? '' : ', ' + pokemon.gender) + (pokemon.set.shiny ? ', shiny' : '');
 		this.add('detailschange', pokemon, pokemon.details);
 		this.add('message', template.baseSpecies + " has Mega Evolved into Mega " + template.baseSpecies + "!");
+		var oldAbility = pokemon.ability;
 		pokemon.setAbility(template.abilities['0']);
 		pokemon.baseAbility = pokemon.ability;
+		this.runEvent('EndAbility', pokemon, oldAbility);
 
 		side.megaEvo = 1;
 		for (var i = 0; i < side.pokemon.length; i++) side.pokemon[i].canMegaEvo = false;
