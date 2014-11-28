72abc7152ed5b19520859a9db8a6168b77646c5b
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-11-28 11:49:01 +0700

    Update Megas For All
    - Mega Evolving does not work so it will be fixed at a later date
    - Update pokedex.js

--- megasforall/v2/battle-engine.js
+++ megasforall/v2/battle-engine.js
@@ -3,10 +3,4 @@
 * "this.item = toId(set.item);", and before
 * line ~203 "this.abilityData = {id: this.ability};"
 */
-this.canMegaEvo = (this.battle.getItem(this.item).megaEvolves === this.baseTemplate.baseSpecies);
-for (var i in this.baseTemplate.otherFormes) {
-	formTemp = this.getTemplate(this.baseTemplate.otherFormes[i]);
-	if (((formTemp.forme === 'Mega-X' && this.item === 'charizarditex') || (formTemp.forme === 'Mega-Y' && this.item === 'charizarditey') || (formTemp.forme === 'Mega' && this.item === 'abomasite')) && formTemp.tier === 'M4A') {
-		this.canMegaEvo = true;
-	}
-}
+// still working on it
