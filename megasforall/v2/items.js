7840835b43fe9bbbf19385134417be317b1b2476
idunnowhy9000 <idunnowhy9000@users.noreply.github.com>
2014-11-29 08:35:09 +0700

    Update items.js

--- megasforall/v2/items.js
+++ megasforall/v2/items.js
@@ -1,56 +1,38 @@
 exports.BattleItems = {
 	"abomasite": {
-		id: "abomasite",
-		name: "Abomasite",
-		spritenum: 575,
-		megaStone: "Abomasnow-Mega",
-		megaEvolves: "Abomasnow",
+		inherit: true,
 		m4aEvolves: "-Mega",
 		onTakeItem: function (item, source) {
 			if (item.megaEvolves === source.baseTemplate.baseSpecies) {
-				if (this.getTemplate(source.baseTemplate.baseSpecies + m4aEvolves).exists) return true;
+				if (this.getTemplate(source.baseTemplate.baseSpecies + m4aEvolves).tier === 'M4A') return true;
 				return false;
 			}
 			return true;
 		},
-		num: 674,
-		gen: 6,
-		desc: "Mega-evolves Abomasnow or holder (if holder has Mega For All's mega)."
+		desc: "Mega-evolves Abomasnow or holder."
 	},
 	"charizarditex": {
-		id: "charizarditex",
-		name: "Charizardite X",
-		spritenum: 585,
-		megaStone: "Charizard-Mega-X",
-		megaEvolves: "Charizard",
+		iinherit: true,
 		m4aEvolves: "-Mega-X",
 		onTakeItem: function (item, source) {
 			if (item.megaEvolves === source.baseTemplate.baseSpecies) {
-				if (this.getTemplate(source.baseTemplate.baseSpecies + m4aEvolves).exists) return true;
+				if (this.getTemplate(source.baseTemplate.baseSpecies + m4aEvolves).tier === 'M4A') return true;
 				return false;
 			}
 			return true;
 		},
-		num: 660,
-		gen: 6,
-		desc: "Mega-evolves Charizard into Mega Charizard X."
+		desc: "Mega-evolves Charizard into Mega Charizard X or holder into it's Mega-X forme."
 	},
 	"charizarditey": {
-		id: "charizarditey",
-		name: "Charizardite Y",
-		spritenum: 586,
-		megaStone: "Charizard-Mega-Y",
-		megaEvolves: "Charizard",
+		inherit: true,
 		m4aEvolves: "-Mega-Y",
 		onTakeItem: function (item, source) {
 			if (item.megaEvolves === source.baseTemplate.baseSpecies) {
-				if (this.getTemplate(source.baseTemplate.baseSpecies + m4aEvolves).exists) return true;
+				if (this.getTemplate(source.baseTemplate.baseSpecies + m4aEvolves).tier === 'M4A') return true;
 				return false;
 			}
 			return true;
 		},
-		num: 678,
-		gen: 6,
-		desc: "Mega-evolves Charizard into Mega Charizard Y."
+		desc: "Mega-evolves Charizard into Mega Charizard Y or holder into it's Mega-Y forme."
 	},
 }
