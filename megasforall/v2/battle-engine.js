59c1306f9f392ef1aeb66e410cdf2ce9e2220ff9
idunnowhy9000 <idunnowhy9000@users.noreply.github.com>
2014-12-04 08:07:40 +0700

    Update battle-engine.js

--- megasforall/v2/battle-engine.js
+++ megasforall/v2/battle-engine.js
@@ -1,6 +1,1 @@
-/*
-* Add this to you battle-engine.js file after line ~202
-* "this.item = toId(set.item);", and before
-* line ~203 "this.abilityData = {id: this.ability};"
-*/
 this.canMegaEvo = ((this.battle.getItem(this.item).megaEvolves === this.baseTemplate.baseSpecies) || (forme && forme.isMega && forme.requiredMove && this.set.moves.indexOf(toId(forme.requiredMove)) > -1) || (this.battle.getTemplate(this.baseTemplate.baseSpecies + this.battle.getItem(this.item).m4aEvolves).tier === 'M4A'));
