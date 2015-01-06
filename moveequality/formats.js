8ae65fcee4d78a000712d26f8f643c61e49fcdc1
idunnowhy9000 <idunnowhy9000@gmail.com>
2015-01-06 17:57:44 +0700

    Update Move Equality

---
+++ moveequality/formats.js
@@ -0,0 +1,8 @@
+	{
+		name: "Move Equality",
+		section: "Other Metagames",
+		
+		mod: "moveequality",
+		ruleset: ["OU"],
+		banlist: ["Greninja", "Dynamic Punch"]
+	}
--- moveequality/scripts.js
+++ moveequality/scripts.js
@@ -3,33 +3,31 @@ exports.BattleScripts = {
 		for (var i in this.data.Moves) {
 			if (this.data.Moves[i].priority === 0) {
 				var basePower = this.data.Moves[i].basePower;
-				if (this.data.Moves[i].multihit === 2) {
-					basePower = 45;
-				} else if (this.data.Moves[i].multihit === [2, 5]) {
-					basePower = 25;
-				} else {
-					basePower = 90;
+				switch (this.data.Moves[i].multihit) {
+					case 2:
+						basePower = 45;
+					break;
+					case 3:
+						basePower = 10;
+					break;
+					case [2, 5]:
+						basePower = 25;
+					break;
+					default:
+						basePower = 90;
+					break;
 				}
 				
 				var basePowerCallback;
 				if (this.data.Moves[i].id === 'triplekick') {
-					basePower = 15;
 					basePowerCallback = function (pokemon) {
 						pokemon.addVolatile('triplekick');
-						return 15 * pokemon.volatiles['triplekick'].hit;
-					};
-				} else if (this.data.Moves[i].id === 'beatup') {
-					// todo
-					basePowerCallback = function (pokemon) {
-						pokemon.addVolatile('beatup');
-						if (!pokemon.side.pokemon[pokemon.volatiles.beatup.index]) return null;
-						return 5 + Math.floor(pokemon.side.pokemon[pokemon.volatiles.beatup.index].template.baseStats.atk / 10);
-					};
+						return 10 * pokemon.volatiles['triplekick'].hit;
+					}
 				}
-				
 				this.modData('Moves', i).basePower = basePower;
 				this.modData('Moves', i).accuracy = 100;
-				if (basePowerCallback) this.modData('Moves', i).basePowerCallback = undefined;
+				if (basePowerCallback) this.modData('Moves', i).basePowerCallback = basePowerCallback;
 				this.modData('Moves', i).onBasePower = undefined;
 			}
 		}
