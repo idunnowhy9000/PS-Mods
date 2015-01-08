dc99e0a18c2ab2dbc5e943b091066d2fe665538a
idunnowhy9000 <idunnowhy9000@gmail.com>
2015-01-08 12:28:33 +0700

    Update 400

--- 400/team-validator.js
+++ 400/team-validator.js
@@ -1,8 +1,4 @@
 if (format.banlistTable && format.banlistTable['400clause']) {
-    var bst;
-    for (var i in template.baseStats) {
-        bst += template.baseStats[i];
-    }
-    if (bst <= 400) return false;
+	return !(Object.sum(template.baseStats) <= 400);
 }
 // (Add this after line ~584 containing "alreadyChecked[template.speciesid] = true;")
