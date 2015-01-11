a1a76b89a8665d6af084d35ae442c21181475efd
idunnowhy9000 <idunnowhy9000@gmail.com>
2015-01-11 16:11:00 +0700

    Update Mega Mania

--- megamania/rulesets.js
+++ megamania/rulesets.js
@@ -2,15 +2,24 @@ exports.BattleFormats = {
 	megamania: {
 		effectType: 'Banlist',
 		validateSet: function (set, format, isNonstandard) {
-			// validate banned abilities
-			var bannedAbilities = {'Arena Trap':1,'Huge Power':1,'Imposter':1,'Parental Bond':1,'Pure Power':1,'Shadow Tag':1,'Wonder Guard':1};
-			if (set.ability in bannedAbilities) {
-				var template = this.getTemplate(set.species || set.name);
-				var legalAbility = false;
-				for (var i in template.abilities) {
-					if (set.ability === template.abilities[i]) legalAbility = true;
+			// validate abilities
+			var template = this.getTemplate(set.species || set.name),
+				legalAbility = false;
+			for (var i in template.abilities) {
+				if (set.ability === template.abilities[i]) legalAbility = true;
+			}
+			if (this.getItem(set.item).id !== "megastone" && !legalAbility) return [(set.name || set.species) + " cannot have " + set.ability + "."];
+			else {
+				// validate banned abilities
+				var bannedAbilities = {'Arena Trap':1,'Huge Power':1,'Imposter':1,'Parental Bond':1,'Pure Power':1,'Shadow Tag':1,'Wonder Guard':1};
+				if (set.ability in bannedAbilities) {
+					
+					
+					for (var i in template.abilities) {
+						
+					}
+					if (!legalAbility && ) return ['The ability ' + set.ability + ' is banned on Pokémon that do not naturally have it.'];
 				}
-				if (!legalAbility && this.getItem(set.item).id === "megastone") return ['The ability ' + set.ability + ' is banned on Pokémon that do not naturally have it.'];
 			}
 		},
 	}
