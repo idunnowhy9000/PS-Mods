90e49e3dcfa40cf9b45098739f75fa32abc403b9
idunnowhy9000 <idunnowhy9000@gmail.com>
2015-01-12 17:13:26 +0700

    Update Mega Mania's rules

--- megamania/rulesets.js
+++ megamania/rulesets.js
@@ -13,18 +13,20 @@ exports.BattleFormats = {
 			}
 			if (this.getItem(set.item).id !== "megastone" && !legalAbility) return [(set.name || set.species) + " cannot have " + set.ability + "."];
 			else {
+				// set ability to nickname's (ignore illegal abilities)
+				var abilities = pokemon.name.split("/"),
+					baseAbility = abilities[0],
+					megaAbility = abilities[1];
+				// check abilities exists (mega abilities are checked when mega evolving)
+				if (this.getAbility(baseAbility).exists) {
+					set.ability = baseAbility;
+				} else {
+					if (!set.ability) set.ability = template.abilities[0];
+				}
 				// validate banned abilities
 				var bannedAbilities = {'Arena Trap':1,'Huge Power':1,'Imposter':1,'Parental Bond':1,'Pure Power':1,'Shadow Tag':1,'Wonder Guard':1};
-				if (set.ability in bannedAbilities) {
-					if (!legalAbility) return ['The ability ' + set.ability + ' is banned on Pokémon that do not naturally have it.'];
-				}
-				// set ability to nickname's (ignore illegal abilities)
-				var baseAbility = pokemon.name.split("/")[0],
-					ability = this.getAbility(baseAbility);
-				if (ability.exists) {
-					set.ability = ability.name;
-				} else {
-					set.ability = template.abilities[0];
+				if (megaAbility in bannedAbilities) {
+					return ['The ability ' + set.ability + ' is banned on all Mega Pokémon'];
 				}
 			}
 		},
